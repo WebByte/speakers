af73b68059375aa2b95c133152accc65c37b3a48
Gadya <zakuson2001@inbox.ru>
2022-01-05 23:01:08 +0300

    modify models.py: add u_activationKey, u_activationKeyExpires to UserProfile; rename userId to userProfileId

--- alex_pron_test_file.py
+++ alex_pron_test_file.py
@@ -1,6 +1,7 @@
 import requests
-
-# r = requests.post('http://127.0.0.1:8000/api/auth/login/', data={'u_login': 'Gadya3','u_password':'12345'})
-r = requests.post('http://127.0.0.1:8000/api/auth/logout/', headers={'Authorization': 'Token 5f5b4b2e092f901b2fce11a3add6e713769f48ad'})
-# r = requests.post('http://127.0.0.1:8000/api/auth/test/')
+# r = requests.post('http://127.0.0.1:8000/auth/signup/', data={'u_email': 'gad222@m.ru','u_password':'12345678'})
+# r = requests.post('https://dev.lectonic.ru/auth/signup/', data={'u_email': 'gad111@m.ru','u_password':'12345678'})
+# r = requests.post('http://127.0.0.1:8000/auth/login/', data={'u_email': 'gad222@m.ru','u_password':'12345678'})
+r = requests.post('http://127.0.0.1:8000/auth/logout/', headers={'Authorization': 'Token 2dfd0da32b9db367b1958726515fb19041685719'})
+# r = requests.post('http://127.0.0.1:8000/auth/test/')
 print (r.text)
